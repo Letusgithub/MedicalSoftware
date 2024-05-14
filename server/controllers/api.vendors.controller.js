d0840d4327d5feb37466872f2100e75013be158d
Atharv140903 <atharvkurde2003@gmail.com>
2024-05-14 19:19:37 +0530

    Duplicate vendor cannot be added to a pharmacy

--- server/controllers/api.vendors.controller.js
+++ server/controllers/api.vendors.controller.js
@@ -3,7 +3,9 @@ const service = require('../services/vendor.service');
 
 exports.createVendor = (req, res) => {
   const data = req.body;
-  service.create(data, (err, results) => {
+  const org_id = data.org_id;
+  const vendor_gstin = data.vendor_gstin;
+  service.checkGstinByOrgId(org_id, vendor_gstin, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(500).json({
@@ -11,10 +13,25 @@ exports.createVendor = (req, res) => {
         message: 'Database connection error',
       });
     }
-    return res.status(200).json({
-      success: 1,
-      results,
-      message: 'Created successfully',
+    if (results.length > 0) {
+      return res.status(200).json({
+        success: 0,
+        message: 'GSTIN already exists',
+      });
+    }
+    service.create(data, (createErr, createResults) => {
+      if (createErr) {
+        console.log(createErr);
+        return res.status(500).json({
+          success: 0,
+          message: 'Database connection error',
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        createResults,
+        message: 'Created successfully',
+      });
     });
   });
 };
